31635e5302fa4b6290f6f6b024424ec13c8dd0a1
hiluan <apt.muenchen@gmail.com>
2022-04-20 16:13:16 -0700

    testing 500x500 with grid

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,4 +1,4 @@
-import Canvas1000x1000 from "../data/Canvas1000x1000";
+// import Canvas1000x1000 from "../data/Canvas1000x1000";
 import Canvas100x100 from "../data/Canvas100x100";
 import Canvas500x500 from "../data/Canvas500x500";
 
@@ -11,11 +11,11 @@ const Test = (props) => {
   //   const test1m = new Array(1000000);
   const c100x100length = Canvas100x100.length;
   const c500x500length = Canvas500x500.length;
-  const canvas1000x1000length = Canvas1000x1000.length;
+  // const canvas1000x1000length = Canvas1000x1000.length;
 
   return (
-    <div onClick={() => console.log(canvas1000x1000length)} id="canvas">
-      {Canvas1000x1000?.map((x) => (
+    <div onClick={() => console.log(c500x500length)} id="canvas">
+      {Canvas500x500?.map((x) => (
         <div>aa</div>
       ))}
     </div>
