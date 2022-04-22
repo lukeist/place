94d96670a42c2bfb657742321c1f624bb7507738
hiluan <apt.muenchen@gmail.com>
2022-04-22 00:13:15 -0700

    testing onMouseEnter & onMouseLeave on pixel

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,15 +2,18 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import Canvas100x100 from "../data/Canvas100x100";
+import Canvas10x10 from "../data/Canvas10x10";
 import Canvas500x500 from "../data/Canvas500x500";
+import Pixel from "./Pixel";
 
 const Canvas = (props) => {
-  const selectedColor = useSelector((state) => state.ultilities.selectedColor);
-  // const [hoveredColor, setHoveredColor] = useState(selectedColor);
-  const [currentColor, setCurrentColor] = useState("#ffffff");
-  const handlerHoveredColor = () => {
-    setCurrentColor(selectedColor);
-  };
+  // const selectedColor = useSelector((state) => state.ultilities.selectedColor);
+  // // const [hoveredColor, setHoveredColor] = useState(selectedColor);
+  // const [currentColor, setCurrentColor] = useState("#ffffff");
+  // const handlerHoveredColor = () => {
+  //   setCurrentColor(selectedColor);
+  //   console.log(selectedColor);
+  // };
   // const resetColor = () => {
   //   setCurrentColor;
   // };
@@ -19,25 +22,38 @@ const Canvas = (props) => {
   const c500x500length = Canvas500x500.length;
   // const canvas1000x1000length = Canvas1000x1000.length;
 
+  const canvas100 = [];
+  for (let i = 1001; i < 9001; i = i + 50) {
+    canvas100.push("#f0" + i);
+  }
   return (
-    <div id="canvas">
-      {Canvas500x500.map((color) => (
-        <div
-          onClick={() => console.log(currentColor)}
-          style={{ backgroundColor: currentColor }}
-          // className="color-pixel"
-          // onMouseOver={(this.style.color = "#0F0")}
-          onMouseEnter={handlerHoveredColor}
-          onMouseLeave={() => setCurrentColor(color)}
-        ></div>
-      ))}
-    </div>
+    // <div id="canvas">
+    //   {canvas100.map((pixelColor) => (
+    //     <div
+    //       onClick={() => console.log(currentColor)}
+    //       style={{ backgroundColor: selectedColor }}
+    //       className="color-pixel"
+    //       // onMouseOver={(this.style.color = "#0F0")}
+    //       onMouseEnter={
+    //         handlerHoveredColor
+    //         // () => console.log(pixelColor)
+    //       }
+    //       // onMouseLeave={() => setCurrentColor(color)}
+    //     ></div>
+    //   ))}
+    // </div>
 
     // <div onClick={() => console.log(canvas1000x1000length)} id="canvas">
     //   {Canvas1000x1000?.map((x) => (
     //     <div>a</div>
     //   ))}
     // </div>
+
+    <div id="canvas">
+      {canvas100.map((pixelColor) => (
+        <Pixel canvas100={canvas100} pixelColor={pixelColor} />
+      ))}
+    </div>
   );
 };
 
--- src/components/Pixel.js
+++ src/components/Pixel.js
@@ -1,35 +1,56 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function Pixel(props) {
-  const { selectedColor } = props;
-  const [pixelColor, setPixelColor] = useState("#fff");
-  const [oldColor, setOldColor] = useState(pixelColor);
-  const [canChangeColor, setCanChangeColor] = useState(true);
+  const { pixelColor, canvas100 } = props;
+  // const { selectedColor } = props;
+  // const [pixelColor, setPixelColor] = useState("#fff");
+  // const [oldColor, setOldColor] = useState(pixelColor);
+  // const [canChangeColor, setCanChangeColor] = useState(true);
 
-  const applyColor = () => {
-    setPixelColor(selectedColor);
-    setCanChangeColor(false);
+  // const applyColor = () => {
+  //   setPixelColor(selectedColor);
+  //   setCanChangeColor(false);
+  // };
+
+  // const changeColorOnHover = () => {
+  //   setOldColor(pixelColor);
+  //   setPixelColor(selectedColor);
+  // };
+
+  // const resetColor = () => {
+  //   if (canChangeColor) {
+  //     setPixelColor(oldColor);
+  //   }
+  //   setCanChangeColor(true);
+  // };
+
+  const selectedColor = useSelector((state) => state.ultilities.selectedColor);
+  // const [hoveredColor, setHoveredColor] = useState(selectedColor);
+  const [currentColor, setCurrentColor] = useState(pixelColor);
+  const handlerHoveredColor = () => {
+    setCurrentColor(selectedColor);
+    console.log(selectedColor);
   };
-
-  const changeColorOnHover = () => {
-    setOldColor(pixelColor);
-    setPixelColor(selectedColor);
-  };
-
-  const resetColor = () => {
-    if (canChangeColor) {
-      setPixelColor(oldColor);
-    }
-    setCanChangeColor(true);
-  };
-
   return (
     <div
-      className="pixel"
-      style={{ backgroundColor: pixelColor }}
-      onClick={applyColor}
-      onMouseEnter={changeColorOnHover}
-      onMouseLeave={resetColor}
+      onClick={() => console.log(currentColor)}
+      style={{ backgroundColor: currentColor }}
+      className="color-pixel"
+      // onMouseOver={(this.style.color = "#0F0")}
+      onMouseEnter={
+        handlerHoveredColor
+        // () => console.log(pixelColor)
+      }
+      onMouseLeave={() => setCurrentColor(pixelColor)}
     ></div>
+
+    // <div
+    //   className="pixel"
+    //   style={{ backgroundColor: pixelColor }}
+    //   onClick={applyColor}
+    //   onMouseEnter={changeColorOnHover}
+    //   onMouseLeave={resetColor}
+    // ></div>
   );
 }
